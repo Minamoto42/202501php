2cc31e743f14ae051bb758e886b58705abf05395
Minamoto42 <ryugenrokuroku@gmail.com>
2025-01-22 19:41:15 +0900

    js practice
    timer

---
+++ frontend/Javascript/code/timer.js
@@ -0,0 +1,29 @@
+let timer = document.getElementById('timer');
+let startButton = document.getElementById('start');
+let stopButton = document.getElementById('stop');
+let resetButton = document.getElementById('reset');
+
+let count = 0;
+let interval;
+
+startButton.addEventListener('click', () => {
+    if (!interval) {
+        interval = setInterval(() => {
+            count++;
+            timer.textContent = count;
+            // timer.innerHTML = count;
+        }, 2000)
+    }
+});
+
+stopButton.addEventListener('click', () => {
+    clearInterval(interval);
+    interval = null;
+});
+
+resetButton.addEventListener('click', () => {
+    clearInterval(interval);
+    interval = null;
+    count = 0;
+    timer.innerHTML = count;
+});
